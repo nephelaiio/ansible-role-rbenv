640eddfd21e072a406bc2b7fba867a70784a91fb
Ted Cook <teodoro.cook@gmail.com>
2017-09-20 20:44:18 -0600

    Add full set of variables for compile test scenario

--- molecule/compile/tests/test_compile.py
+++ molecule/compile/tests/test_compile.py
@@ -5,23 +5,17 @@ import testinfra.utils.ansible_runner
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
-versions = ['2.4.1']
+
+ruby_version = '2.4.1'
 
 
-def test_rbenv_bin(Command):
-    cmd = '/bin/bash -l -c "rbenv --version"'
-    assert Command(cmd).rc == 0
+def test_rbenv_version(host):
+    cmd = '~/.rbenv/bin/rbenv versions --bare'
+    with host.sudo('rbenv'):
+        assert host.check_output(cmd) == ruby_version
 
 
-def test_rbenv_version(Command):
-    for version in versions:
-        cmd_tpl = "/bin/bash -l -c \"rbenv versions --bare | grep {0}\""
-        cmd = cmd_tpl.format(version)
-        assert Command(cmd).rc == 0
-
-
-def test_rbenv_local(Command):
-    for version in versions:
-        cmd_tpl = "/bin/bash -l -c \"rbenv local | grep {0}\""
-        cmd = cmd_tpl.format(version)
-        assert Command(cmd).rc == 0
+def test_rbenv_local(host):
+    cmd = '~/.rbenv/versions/2.4.1/bin/ruby --version'
+    with host.sudo('rbenv'):
+        assert host.run(cmd).rc == 0
