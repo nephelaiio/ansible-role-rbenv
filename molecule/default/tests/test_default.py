8a52403f1602a4f5da4b3354deafb01063d2869f
Ted Cook <teodoro.cook@gmail.com>
2019-07-21 23:11:59 -0600

    Fix test syntax error

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,6 +6,6 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_rbenv_bin(host.command):
+def test_rbenv_bin(host):
     cmd = '/bin/bash -l -c "rbenv --version"'
     assert host.command(cmd).rc == 0
