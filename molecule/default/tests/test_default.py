4785296ddb86c29eaf45a67eaf3a74ca32157d33
Ted Cook <teodoro.cook@gmail.com>
2019-07-21 23:10:19 -0600

    Fix testinfra deprecation errors

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,6 +6,6 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_rbenv_bin(Command):
+def test_rbenv_bin(host.command):
     cmd = '/bin/bash -l -c "rbenv --version"'
-    assert Command(cmd).rc == 0
+    assert host.command(cmd).rc == 0
